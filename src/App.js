a4e429fe9ac8e087bfabe1665c49b2ada6a18751
Alicia Collins <alicia.collins60@me.com>
2018-08-27 21:30:16 -0700

    updating saved files

--- src/App.js
+++ src/App.js
@@ -2,19 +2,7 @@
 import React from 'react';
 import Game from "./components/Game"
 
-
 const App = () => <Game/>
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//          <h1 className="App-title">Clicky-Game</h1>
-//         </header>
-        
-//       </div>
-//     );
-//   }
-// }
+
 
 export default App;
